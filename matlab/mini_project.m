59e93d036df7d22790de4417639988d80d6fed07
williamparker <baiweilian@gmail.com>
2021-04-21 08:42:57 -0500

    Added mini-project template files

---
+++ matlab/mini_project.m
@@ -0,0 +1,18 @@
+% Comments describing mini-project ~ 100-200 words
+
+% Simulation parameters
+%	These are values particular to the simulation 
+%	that do not change later in the script
+
+% Computed parameters (from simulation parameters)
+%	These are values that do not change later in the script
+%	and are calculated from formulas using the simulation parameters
+
+% Function calls and simple calculations for:
+%	data read-in
+%	simulation solution 
+%	visualization
+
+% Function definitions for simulation solution & visualization
+%	Each function contains help text: https://www.mathworks.com/help/matlab/matlab_prog/add-help-for-your-program.html
+
---
+++ python/mini_project.py
@@ -0,0 +1,22 @@
+# Comments describing mini-project ~ 100-200 words
+
+# All code is inside function definitions for simulation solution & visualization (functional programming)
+#	Each function contains a docstring compliant with PEP 257: https://www.python.org/dev/peps/pep-0257/
+#	Module ends with if __name__ == "__main__" block to execute central function of the code
+
+# Main simulation function structure
+#	Module imports
+#		Best practice: import numpy as np and use np. prefix in
+#				front of all NumPy functions
+#		Use SciPy constants for physical constants
+#			https://docs.scipy.org/doc/scipy/reference/constants.html
+#		Follow best practice order: https://docs.python.org/3/faq/programming.html#what-are-the-best-practices-for-using-import-in-a-module
+# 	Simulation parameters
+#		Each parameter named clearly and units marked in in-line comment
+#		Naming of all variables should comply with PEP 8: https://www.python.org/dev/peps/pep-0008/#documentation-strings
+#			(lower_case_with_underscores)
+# 	Computed parameters (from simulation parameters)
+# 	Function calls (use PEP 8-compliant lower_case_with_underscores) and simple calculations for:
+#		data read-in
+#		simulation solution 
+#		visualization
