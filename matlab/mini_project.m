25a9b09ea349c99954e048da587953b90191ced6
William Parker <parkerw@uwp.edu>
2022-04-05 10:27:40 -0500

    Update mini_project.m

--- matlab/mini_project.m
+++ matlab/mini_project.m
@@ -1,5 +1,6 @@
 %%%% RENAME from mini_project.m to (your_project_short_name).m
 % Comments describing mini-project ~ 100-200 words
+% (revise from initial written description removing motivation and references to yourself)
 
 % Simulation parameters
 %	These are values particular to the simulation 
@@ -13,8 +14,10 @@
 %	data read-in
 %	simulation solution 
 %	visualization
+% 
 
 % Function definitions for simulation solution & visualization
 %	Each function contains help text: https://www.mathworks.com/help/matlab/matlab_prog/add-help-for-your-program.html
+% (see documentation on local script functions: https://www.mathworks.com/help/matlab/matlab_prog/local-functions-in-scripts.html)
 
-%
+% You have the option to contain all functions in this script or make separate scripts with functions if you find this one getting too long
